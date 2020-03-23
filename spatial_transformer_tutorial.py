db420c7ca91ab5e80c49d412fea6d11e57677338
vinteuil <zcapmav@ucl.ac.uk>
2020-03-23 14:31:23 +0000

    :construction:

--- spatial_transformer_tutorial.py
+++ spatial_transformer_tutorial.py
@@ -2,29 +2,20 @@
 # Author: Ghassen Hamrouni
 
 from __future__ import print_function
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 import torch.optim as optim
 import torchvision
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
-
-
-import torch
-import torch.nn as nn
 import torchvision.models as models
-
+from transformer import Net
+from discriminator import DiscriminatorNet
 plt.ion()  # interactive mode
-
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -57,120 +48,12 @@ test_loader = torch.utils.data.DataLoader(
 )
 
 
-class Flatten(nn.Module):
-    def __init__(self):
-        super(Flatten, self).__init__()
-
-    def forward(self, x):
-        return x.view(x.size(0), -1)
 
 
-### help from https://github.com/aicaffeinelife/Pytorch-STN/blob/master/models/STNModule.py
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-        # Spatial transformer localization-network
-        self.localization = nn.Sequential(
-            nn.Conv2d(3, 6, 5),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(6, 16, 5),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, 2),
-        )
-
-        # Spatial transformer localization-network
-        self.colorization = nn.Sequential(
-            nn.Conv2d(3, 6, 5),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, 2),
-            nn.Conv2d(6, 16, 5),
-            nn.ReLU(True),
-            nn.MaxPool2d(2, 2),
-            Flatten(),
-            nn.Linear(400, 3),
-        )
-
-        # Regressor for the 3 * 2 affine matrix
-        self.fc_loc = nn.Sequential(
-            nn.Linear(400, 64), nn.ReLU(True), nn.Linear(64, 3 * 2)
-        )
-
-        # Initialize the weights/bias with identity transformation
-        self.fc_loc[2].weight.data.zero_()
-        self.fc_loc[2].bias.data.copy_(
-		torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float)
-        )
-
-    # Spatial transformer network forward function
-    def stn(self, x):
-
-        xs = self.localization(x)
-        xs = xs.view(-1, 16 * 5 * 5)
-        theta = self.fc_loc(xs)
-        theta = theta.view(-1, 2, 3)
-
-        return x, theta
-
-    def color_transformer(self, x):
-        color_alter_params = self.colorization(x)
-        transformed = torch.zeros(x.size())
-
-        for i in range(x.size()[0]):
-            for j in range(3):
-                transformed[i, j, :, :] = x[i, j, :, :] * color_alter_params[i][j]
-
-        return transformed
-
-    def transform_with_theta(self, x, theta):
-        grid = F.affine_grid(theta, x.size())
-        x = F.grid_sample(x, grid)
-        return x
-
-    def transform(self, x):
-        x, theta = self.stn(x)
-        x = self.transform_with_theta(x, theta)
-        x = self.color_transformer(x)
-        return x
-
-    def forward(self, x):
-        # transform the input
-        x = self.transform(x)
-
-        # Perform the usual forward pass
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
 
 
 model = Net().to(device)
 
-
-class DiscriminatorNet(nn.Module):
-    def __init__(self):
-        super(DiscriminatorNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 2)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
 
 
 optimizer = optim.SGD(model.parameters(), lr=0.01)
@@ -270,7 +153,7 @@ for epoch in range(1, 100):
     # Visualize the STN transformation on some input batch
     torch.save(model.state_dict(), "temp_model.pt")
     visualize_stn()
-
+    
     plt.ioff()
     plt.savefig(str(epoch) + "_example.png")
     plt.close()
---
+++ transformer.py
@@ -0,0 +1,79 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+### help from https://github.com/aicaffeinelife/Pytorch-STN/blob/master/models/STNModule.py
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        batch_size = x.shape[0]
+        return x.view(batch_size, -1)
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+        # Spatial transformer localization-network
+        self.localization = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+        )
+
+        # Spatial transformer localization-network
+        self.colorization = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            Flatten(),
+            nn.Linear(400, 3),
+        )
+
+        # Regressor for the 3 * 2 affine matrix
+        self.fc_loc = nn.Sequential(
+            nn.Linear(400, 64), nn.ReLU(True), nn.Linear(64, 3 * 2)
+        )
+
+        # Initialize the weights/bias with identity transformation
+        self.fc_loc[2].weight.data.zero_()
+        self.fc_loc[2].bias.data.copy_(
+		torch.tensor([1, 0, 0, 0, 1, 0], dtype=torch.float)
+        )
+
+    # Spatial transformer network forward function
+    def stn(self, x):
+
+        xs = self.localization(x)
+        xs = xs.view(-1, 16 * 5 * 5)
+        theta = self.fc_loc(xs)
+        theta = theta.view(-1, 2, 3)
+
+        return x, theta
+
+    def color_transformer(self, x):
+        color_alter_params = self.colorization(x)
+        transformed = torch.zeros(x.size())
+
+        for i in range(x.size()[0]):
+            for j in range(3):
+                transformed[i, j, :, :] = x[i, j, :, :] * color_alter_params[i][j]
+
+        return transformed
+
+    def transform_with_theta(self, x, theta):
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+        return x
+
+    def transform(self, x):
+        x, theta = self.stn(x)
+        x = self.transform_with_theta(x, theta)
+        x = self.color_transformer(x)
+        return x
