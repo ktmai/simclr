83de61cf861e63692af302eb94e8a4a1c4aabe6b
self-supervisor <augustine.mavor-parker.15@ucl.ac.uk>
2020-03-23 23:09:39 +0000

    validation loader

--- data_utils.py
+++ data_utils.py
@@ -1,5 +1,6 @@
 from torchvision import datasets, transforms
 import torch
+from torch.utils.data import DataLoader, sampler
 
 # Training dataset
 train_loader = torch.utils.data.DataLoader(
@@ -15,3 +16,31 @@ train_loader = torch.utils.data.DataLoader(
     shuffle=True,
     num_workers=4,
 )
+
+# https://github.com/pytorch/vision/issues/168
+class ChunkSampler(sampler.Sampler):
+    """Samples elements sequentially from some offset. 
+    Arguments:
+        num_samples: # of desired datapoints
+        start: offset where we should start selecting from
+    """
+    def __init__(self, num_samples, start = 0):
+        self.num_samples = num_samples
+        self.start = start
+
+    def __iter__(self):
+        return iter(range(self.start, self.start + self.num_samples))
+
+    def __len__(self):
+        return self.num_samples
+
+NUM_TRAIN = 49000
+NUM_VAL = 1000
+
+cifar10_train = datasets.CIFAR10('./cs231n/datasets', train=True, download=True,
+                           transform=torch.ToTensor())
+loader_train = DataLoader(cifar10_train, batch_size=64, sampler=ChunkSampler(NUM_TRAIN, 0))
+
+cifar10_val = datasets.CIFAR10('./cs231n/datasets', train=True, download=True,
+                           transform=torch.ToTensor())
+loader_val = DataLoader(cifar10_val, batch_size=64, sampler=ChunkSampler(NUM_VAL, NUM_TRAIN))
